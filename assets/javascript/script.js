e9c32df723f606bb3214494331769f3527778921
c-andres <charlesalexanderandres@gmail.com>
2017-09-06 23:44:56 -0700

    added logic to get luigi into opponent container

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -29,30 +29,81 @@ for (var i = 0; i < 4; i++) {
 		$(hpId[i]).text(characters[i].hp);
 };
 
-$( "#mario" ).click(function() {
-	$("#selectCharacter" ).addClass("hide");
-	$("#luigi, #toad, #bowser").appendTo("#opponentChoicesRow");
- 	$("#selectOpponent, #opponentChoices").addClass("show");
- 	$("#characterChoices").html("Your Character"); 
+$( "#mario" ).click(function(){
+	if  ($("#mario").hasClass("inCharacterChoices")) {
+		$("#selectCharacter" ).addClass("hide");
+		$("#luigi, #toad, #bowser").appendTo("#opponentChoicesRow").addClass("inOpponentChoices").removeClass("inCharacterChoices");
+	 	$("#selectOpponent, #opponentChoices").addClass("show");
+	 	$("#characterChoices").html("Your Character");
+	 	$("#mario").addClass("inOpponentChoices");
+	 	$("#mario").removeClass("inCharacterChoices");
+
+	}
+	else if ($("#mario").hasClass("inOpponentChoices")){
+
+		$("#mario").appendTo("#opponentRow");
+		$("#mario").removeClass("inOpponentChoices");
+	 	$("#mario").addClass("inOpponent");
+	}
+	else {
+		return nothing;
+	}
 });
 $( "#luigi" ).click(function() {
-	$("#selectCharacter" ).addClass("hide");
-	$("#mario, #toad, #bowser").appendTo("#opponentChoicesRow");
- 	$("#selectOpponent, #opponentChoices").addClass("show");
- 	$("#characterChoices").html("Your Character");
+	if ($("#luigi").hasClass("inCharacterChoices")){
+		$("#selectCharacter" ).addClass("hide");
+		$("#mario, #toad, #bowser").appendTo("#opponentChoicesRow").addClass("inOpponentChoices").removeClass("inCharacterChoices");
+	 	$("#selectOpponent, #opponentChoices").addClass("show");
+	 	$("#characterChoices").html("Your Character");
+	 	$("#luigi").removeClass("inCharacterChoices");
+	 	$("#luigi").addClass("inOpponentChoices");
+	 }
+	else if ($("#luigi").hasClass("inOpponentChoices")){
+		$("#opponentContainer").addClass("show");
+		$("#luigi").appendTo("#opponentRow");
+		$("#luigi").removeClass("inOpponentChoices");
+	 	$("#luigi").addClass("inOpponent");
+	}
+	else {
+		return nothing;
+	}
 });
 $( "#bowser" ).click(function() {
-	$("#selectCharacter" ).addClass("hide");
-	$("#mario, #luigi, #toad").appendTo("#opponentChoicesRow");
- 	$("#selectOpponent, #opponentChoices").addClass("show");
- 	$("#characterChoices").html("Your Character");
+	if ($("#bowser").hasClass("inCharacterChoices")){
+		$("#selectCharacter" ).addClass("hide");
+		$("#mario, #luigi, #toad").appendTo("#opponentChoicesRow");
+	 	$("#selectOpponent, #opponentChoices").addClass("show");
+	 	$("#characterChoices").html("Your Character");
+	 	$("#bowser").removeClass("inCharacterChoices");
+	 	$("#bowser").addClass("inOpponentChoices");
+	 }
+	else if ($("#bowser").hasClass("inOpponentChoices")){
+		$("#bowser").appendTo("#opponentRow");
+		$("#bowser").removeClass("inOpponentChoices");
+	 	$("#bowser").addClass("inOpponent");
+	}
+	else {
+		return nothing;
+	}
 });
 $( "#toad" ).click(function() {
-	$("#selectCharacter" ).addClass("hide");
-	$("#mario, #luigi, #bowser").appendTo("#opponentChoicesRow");
- 	$("#selectOpponent, #opponentChoices").addClass("show");
- 	$("#characterChoices").html("Your Character");
-});  
+	if ($("#toad").hasClass("inCharacterChoices")){
+		$("#selectCharacter" ).addClass("hide");
+		$("#mario, #luigi, #bowser").appendTo("#opponentChoicesRow");
+	 	$("#selectOpponent, #opponentChoices").addClass("show");
+	 	$("#characterChoices").html("Your Character");
+	 	$("#toad").removeClass("inCharacterChoices");
+	 	$("#toad").addClass("inOpponentChoices");
+	 }
+	else if ($("#toad").hasClass("inOpponentChoices")){
+		$("#toad").appendTo("#opponentRow");
+		$("#toad").removeClass("inOpponentChoices");
+	 	$("#toad").addClass("inOpponent");
+	}
+	else {
+		return nothing;
+	}
+});
 
 
 
